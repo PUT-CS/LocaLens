51824eaacb70f2b4b1f69c97b53fcbd5b26a8d3b
Michał Miłek <michal.mileq@gmail.com>
2024-03-11 23:12:45 +0100

    Add Google sign in, Google maps view

--- LocaLensApp/App.tsx
+++ LocaLensApp/App.tsx
@@ -1,14 +1,87 @@
 import * as React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, ScrollView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MapView, { enableLatestRenderer } from "react-native-maps";
+import { style } from "./src/mapStyle";
+import { GoogleSigninButton } from "@react-native-google-signin/google-signin";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  GoogleSignin,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
 
-function HomeScreen({ navigation }) {
+enum AppScreens {
+  Login = "Login",
+  Dashboard = "Dashboard",
+  Map = "Map",
+  MyPins = "My Pins",
+}
+
+GoogleSignin.configure({
+  webClientId: process.env.WEB_ID,
+  iosClientId: process.env.IOS_ID,
+});
+
+WebBrowser.maybeCompleteAuthSession();
+
+function Login({ navigation }) {
+  const loginOnPress = async () => {
+    try {
+      console.log("pressed");
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log(JSON.stringify(userInfo, null, 2));
+      setUserInfo(userInfo);
+      navigation.replace("Dashboard");
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log("sign in cancelled");
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log("sign in in progress");
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log("play services not available");
+        // play services not available or outdated
+      } else {
+        console.log("other error");
+        // some other error happened
+      }
+      console.error(error);
+    }
+  };
+
+  const [userInfo, setUserInfo] = React.useState(null);
+  return (
+    <ScrollView>
+      <Text>Login</Text>
+      <GoogleSigninButton
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={loginOnPress}
+      />
+      <Text>{JSON.stringify(userInfo, null, 2)}</Text>
+    </ScrollView>
+  );
+}
+
+function Dashboard({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
-      <Button title="Go to Map" onPress={() => navigation.navigate("Map")}>
-        <Text>Go to Map</Text>
+      <Button
+        title={AppScreens.Map}
+        onPress={() => navigation.navigate(AppScreens.Map)}
+      >
+        <Text>{AppScreens.Map}</Text>
+      </Button>
+      <Button
+        title={AppScreens.MyPins}
+        onPress={() => navigation.navigate(AppScreens.MyPins)}
+      >
+        <Text>{AppScreens.MyPins}</Text>
       </Button>
     </View>
   );
@@ -16,8 +89,16 @@ function HomeScreen({ navigation }) {
 
 function MapScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Map Screen</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map} customMapStyle={style} />
+    </View>
+  );
+}
+
+function MyPins() {
+  return (
+    <View>
+      <Text>Fetch User Pins</Text>
     </View>
   );
 }
@@ -28,11 +109,17 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name={AppScreens.Login} component={Login} />
+        <Stack.Screen name={AppScreens.Dashboard} component={Dashboard} />
         <Stack.Screen
-          name="Map"
+          name={AppScreens.Map}
           component={MapScreen}
-          options={{ title: "Map" }}
+          options={{ title: AppScreens.Map }}
+        />
+        <Stack.Screen
+          name={AppScreens.MyPins}
+          component={MyPins}
+          options={{ title: AppScreens.MyPins }}
         />
       </Stack.Navigator>
     </NavigationContainer>
@@ -41,15 +128,14 @@ function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#222222",
+    ...StyleSheet.absoluteFillObject,
+    height: "100%",
+    width: "100%",
+    justifyContent: "flex-end",
     alignItems: "center",
-    justifyContent: "center",
   },
-  button: {
-    padding: "3%",
-    backgroundColor: "blue",
-    borderRadius: 20,
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
 
